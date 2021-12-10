9aa8e782f96f05fe159be1545ada103325f77c7d
hayeong0206 <76735412+hayeong0206@users.noreply.github.com>
2021-12-10 14:06:29 +0900

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
-<<<<<<< Updated upstream
 import React, {useContext, useState} from 'react';
-=======
-import React, {useState} from 'react';
->>>>>>> Stashed changes
 import {StatusBar, Text, Dimensions, ScrollView, View} from 'react-native';
 import { SafeAreaInsetsContext, SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 import { ViewStyles, textStyles } from './styles';
-import Input from './components/Input';
-import { images } from './images';
-import IconButton from './components/IconButton';
-import Task from './components/Task';
 import Calendar from './components/Calendar';
 import { theme } from './theme';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import SearchEngine from './components/Search_App'
 import { ScreenStack } from 'react-native-screens';
-<<<<<<< Updated upstream
 import Category from './components/Category';
 import Main from './components/Main';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-=======
-import { TouchableOpacity } from 'react-native-gesture-handler';
 
->>>>>>> Stashed changes
 
 function CalendarScreen(){
     return (
@@ -41,7 +29,6 @@ function SearchScreen(){
     )
 }
 
-<<<<<<< Updated upstream
 function categoryScreen(){
     return (
         <SafeAreaView style = {ViewStyles.container}>
@@ -50,13 +37,15 @@ function categoryScreen(){
     )
 }
 
-function mainScreen(){
+function mainScreen(navigation, route){
+    //const{navigate} = this.props.navigation;
     return (
         <SafeAreaView style = {ViewStyles.container}>
-            <Main />
+            <Main navigation={navigation} route={route}/>
         </SafeAreaView>
     )
 }
+
 
 const Stack = createStackNavigator();
 
@@ -64,57 +53,6 @@ const Stack = createStackNavigator();
 
 
 export default function App(props){
-=======
-const Stack = createStackNavigator();
-
-export default function App(){
-
-    const width = Dimensions.get('window').width;
-    const [newTask, setNewTask] = useState('');
-    
-
-    const [tasks, setTasks] = useState({
-        '1' : {id: '1', text: "Todo item #1", completed: false},
-        '2' : {id: '2', text: "Todo item #2", completed: false},
-    });
-
-    const _addTask = () => {
-        alert(`Add: ${newTask}`);
-        const ID = Date.now().toString();
-        const newTaskObject = {
-            [ID]: {id: ID, text: newTask, completed: false},
-        };
-        setNewTask('');
-        setTasks({...tasks, ...newTaskObject});
-    }
-
-    const _deleteTask = id => {
-        const currentTasks = Object.assign({}, tasks);
-        delete currentTasks[id];
-        setTasks(currentTasks);
-    };
-
-    const _toggleTask = id => {
-        const currentTasks = Object.assign({}, tasks);
-        currentTasks[id]['completed'] = !currentTasks[id]['completed'];
-        setTasks(currentTasks);
-    };
-
-    const _updateTask = item => {
-        const currentTasks = Object.assign({}, tasks);
-        currentTasks[item.id] = item;
-        setTasks(currentTasks);
-    };
-
-
-    const _onBlur = () => {
-        setNewTask('');
-    };
-
-    const _handleTextChange = text => {
-        setNewTask(text);
-    };
->>>>>>> Stashed changes
 
 
     return (
@@ -124,18 +62,10 @@ export default function App(){
                 <Stack.Navigator initialRouteName="calendar" screenOptions = {{headerShown: false}}>
                     <Stack.Screen name = "calendar" component = {CalendarScreen} />
                     <Stack.Screen name = "SearchScreen" component = {SearchScreen} />
-<<<<<<< Updated upstream
                     <Stack.Screen name = "CategoryScreen" component = {categoryScreen} />
-                    <Stack.Screen name = "MainScreen" component = {mainScreen} />
-=======
->>>>>>> Stashed changes
+                    <Stack.Screen name = {"MainScreen"} component ={mainScreen} />
                 </Stack.Navigator>
         </NavigationContainer>
         </SafeAreaProvider>
-       
     );
-<<<<<<< Updated upstream
 };
-=======
-};
->>>>>>> Stashed changes
