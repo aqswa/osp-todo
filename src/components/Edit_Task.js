4de72da078db9e856018e3dccf7a34294aff9d64
cham1ng <88438580+cham1ng@users.noreply.github.com>
2021-12-11 15:04:37 +0900

    Update Edit_Task.js

--- src/components/Edit_Task.js
+++ src/components/Edit_Task.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View, Text} from 'react-native';
 import {theme} from '../theme';
 import PropTypes from 'prop-types';
